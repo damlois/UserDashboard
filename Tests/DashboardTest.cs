4f5bfc0aaedc686bcdf8c0e80668e7e574619df8
Lois Adegbohungbe <lois0168@gmail.com>
2021-01-17 22:03:40 +0100

    added tests

--- Tests/DashboardTest.cs
+++ Tests/DashboardTest.cs
@@ -14,58 +14,99 @@ namespace Tests
 {
     public class DashboardTest
     {
+        #region Index
         [Fact]
         public void Index_ReturnsAViewResult_WithAListOfUsers()
         {
-            // Arrange
             var mockRepo = new Mock<IDataRepository>();
-            mockRepo.Setup(repo => repo.GetMultipleUsers(3, null))
-                .Returns(GetTestUsers());
-            var controller = new HomeController(mockRepo.Object);
+            mockRepo.Setup(repo => repo.GetMultipleUsers(It.IsAny<int>(), It.IsAny<string>()))
+                .Returns<int, string>((x, y) => Task.FromResult(GetTestUsers()));
+            var mockTempData = new Mock<ITempDataDictionary>();
 
-            // Act
+            var controller = new HomeController(mockRepo.Object);
+            controller.TempData = mockTempData.Object;
+
             var result = controller.Index(null, 1).GetAwaiter().GetResult();
 
-            // Assert
             var viewResult = Assert.IsType<ViewResult>(result);
             var model = Assert.IsAssignableFrom<List<User>>(
                 viewResult.ViewData.Model);
             Assert.Equal(3, model.Count());
         }
 
-        private async Task<List<User>> GetTestUsers()
+        private List<User> GetTestUsers()
+        {
+            var randomUsers = new List<User>
+            {
+                new User
+                {
+                    Name = new UserName { First = "Kayode", Last = "Tola" },
+                    Email = "kayodetola@gmail.com",
+                    Gender = "Male",
+                    Phone = "08100237845",
+                    Picture = new UserPicture { Large = "kayode@url.com" }
+                },
+                new User
+                {
+                    Name = new UserName { First = "Tunji", Last = "Idowu" },
+                    Email = "tunjiidowu@gmail.com",
+                    Gender = "Female",
+                    Phone = "08100237843",
+                    Picture = new UserPicture { Large = "tunji@url.com" }
+                },
+                new User
+                {
+                    Name = new UserName { First = "Tola", Last = "Eben" },
+                    Email = "tolaeben@gmail.com",
+                    Gender = "Male",
+                    Phone = "070123489422",
+                    Picture = new UserPicture { Large = "tola@url.com" }
+                }
+            };
+
+            return randomUsers;
+        }
+
+        [Fact]
+        public void Index_ReturnsSameUser_CheckRandomUserAfterPagination()
         {
             var mockRepo = new Mock<IDataRepository>();
-            return await mockRepo.Object.GetMultipleUsers(3, "female");
-            //var randomUsers = new List<User>
-            //{
-            //    new User
-            //    {
-            //        Name = new UserName { First = "Kayode", Last = "Tola" },
-            //        Email = "kayodetola@gmail.com",
-            //        Gender = "Male",
-            //        Phone = "08100237845",
-            //        Picture = new UserPicture { Large = "kayode@url.com" }
-            //    },
-            //    new User
-            //    {
-            //        Name = new UserName { First = "Tunji", Last = "Idowu" },
-            //        Email = "tunjiidowu@gmail.com",
-            //        Gender = "Female",
-            //        Phone = "08100237843",
-            //        Picture = new UserPicture { Large = "tunji@url.com" }
-            //    },
-            //    new User
-            //    {
-            //        Name = new UserName { First = "Tola", Last = "Eben" },
-            //        Email = "tolaeben@gmail.com",
-            //        Gender = "Male",
-            //        Phone = "070123489422",
-            //        Picture = new UserPicture { Large = "tola@url.com" }
-            //    }
-            //};
+            mockRepo.Setup(repo => repo.GetMultipleUsers(It.IsAny<int>(), It.IsAny<string>()))
+                .Returns<int, string>((x, y) => Task.FromResult(GetTestUsers()));
+            var mockTempData = new Mock<ITempDataDictionary>();
 
-            //return randomUsers;
+            var controller = new HomeController(mockRepo.Object);
+            controller.TempData = mockTempData.Object;
+
+            var result1 = controller.Index(null, 1).GetAwaiter().GetResult();
+            var viewResult1 = Assert.IsType<ViewResult>(result1);
+
+            var result2 = controller.Index(null, 2).GetAwaiter().GetResult();
+            var viewResult2 = Assert.IsType<ViewResult>(result2);
+
+            var areEqual = viewResult1.TempData["randomUser"] == viewResult2.TempData["randomUser"];
+            Assert.True(areEqual);
         }
+
+        [Fact]
+        public void Index_ReturnsAViewResult_WithNoUser()
+        {
+            var mockRepo = new Mock<IDataRepository>();
+            mockRepo.Setup(repo => repo.GetMultipleUsers(It.IsAny<int>(), It.IsAny<string>()))
+                .Returns<int, string>((x, y) => Task.FromResult<List<User>>(null));
+            var mockTempData = new Mock<ITempDataDictionary>();
+
+            var controller = new HomeController(mockRepo.Object);
+            controller.TempData = mockTempData.Object;
+
+            var result = controller.Index(null, 1).GetAwaiter().GetResult();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<User>>(
+                viewResult.ViewData.Model);
+            Assert.Empty(model);
+        }
+        #endregion
+
     }
 }
